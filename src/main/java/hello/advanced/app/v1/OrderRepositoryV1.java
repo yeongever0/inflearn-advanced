41ddefc09b253a664d70540f4a189dc544bf6280
yeongever0 <yeongever0@gmail.com>
2024-03-10 23:58:52 +0900

    add: 로그추적기 V1 적용

---
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,38 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.helloTrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) throws IllegalAccessException {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.request()");
+            if (itemId.equals("ex")) {
+                throw new IllegalAccessException("예외 발생!");
+            }
+            sleep(1000);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw  e;
+        }
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
